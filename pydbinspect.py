1cb9d396c71519a121c22ed44456233d58b63490
gruszczy <gruszczy@localhost>
2010-12-08 21:34:37 +0100

    feature: inspecting explicit column names in postgresql

--- pydbinspect.py
+++ pydbinspect.py
@@ -11,13 +11,13 @@ class DatabaseInspector:
         return [row[0] for row in self._select(self._TABLE_NAMES_SQL)]
         
     @abstractmethod
-    def get_fields(self, table):
+    def get_columns(self, table):
         pass
     
     def _select(self, sql):
         connection = self._api.connect(*self._db_params)
         cursor = connection.cursor()
-        cursor.execute(self._TABLE_NAMES_SQL)
+        cursor.execute(sql)
         rows = list(cursor)
         connection.close()
         return rows
@@ -32,24 +32,31 @@ class SqliteInspector(DatabaseInspector):
         import sqlite3
         self._api = sqlite3
         
-    def get_fields(self, table):
+    def get_columns(self, table):
         pass
 
 
 class PostgresInspector(DatabaseInspector):
     
-    _TABLE_NAMES_SQL = "SELECT table_name FROM information_schema.tables " \
-                        "WHERE table_schema = 'public'"
+    _TABLE_NAMES_SQL = """SELECT table_name 
+                          FROM information_schema.tables 
+                          WHERE table_schema = 'public'"""
+    _COLUMN_NAMES_SQL = """SELECT column_name 
+                           FROM information_schema.columns
+                           WHERE table_name = '%s'"""
     
     def __init__(self, *db_params):
         DatabaseInspector.__init__(self, *db_params)
         import psycopg2
         self._api = psycopg2
         
-    def get_fields(self, table):
-        pass
+    def get_columns(self, table):
+        sql = self._COLUMN_NAMES_SQL % table
+        return [row[0] for row in self._select(sql)]
         
 
 if __name__ == "__main__":
     print SqliteInspector("test.db3").get_tables()
-    print PostgresInspector("dbname=django user=django").get_tables()
+    pgsql = PostgresInspector("dbname=django user=django")
+    for table in pgsql.get_tables():
+        print table, pgsql.get_columns(table)
