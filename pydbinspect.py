9d2a29710de1d4d94711936d182e5997f1372a54
gruszczy <gruszczy@localhost>
2010-12-10 14:45:12 +0100

    feature: retrieving column names for sqlite3

--- pydbinspect.py
+++ pydbinspect.py
@@ -1,5 +1,10 @@
-import sys
+#!/usr/bin/python
+
 from abc import ABCMeta, abstractmethod
+from re import compile as re_compile, search as re_search
+
+class InspectError(Exception):
+    pass
 
 class DatabaseInspector:
     __metaclass__ = ABCMeta
@@ -25,15 +30,32 @@ class DatabaseInspector:
 
 class SqliteInspector(DatabaseInspector):
     
-    _TABLE_NAMES_SQL = "SELECT name FROM sqlite_master"
+    _TABLE_NAMES_SQL = """SELECT name 
+                          FROM sqlite_master"""
     
+    _COLUMN_NAMES_SQL = """SELECT sql
+                           FROM sqlite_master
+                           WHERE name = '%s'"""
+
     def __init__(self, *db_params):
         DatabaseInspector.__init__(self, *db_params)
         import sqlite3
         self._api = sqlite3
         
     def get_columns(self, table):
-        pass
+        sql = self._COLUMN_NAMES_SQL % table
+        # only one row should be returned with only one value
+        table_sql = self._select(sql)[0][0]
+        return self._parse_table_sql(table_sql)
+        
+    def _parse_table_sql(self, sql):
+        start = sql.find('(')
+        end = sql.rfind(')')
+        if start == -1 or end == -1:
+            raise InspectError("Failed to parse table sql.")
+        columns = [col.strip() for col in sql[start + 1:end].split(',')]
+        columns = [column.split(' ')[0] for column in columns]
+        print columns
 
 
 class PostgresInspector(DatabaseInspector):
@@ -56,7 +78,12 @@ class PostgresInspector(DatabaseInspector):
         
 
 if __name__ == "__main__":
-    print SqliteInspector("test.db3").get_tables()
+    print 'sqlite3'
+    sqlite = SqliteInspector("test.db3")
+    for table in sqlite.get_tables():
+        print table, sqlite.get_columns(table)
     pgsql = PostgresInspector("dbname=django user=django")
+    print 'postgresql'
     for table in pgsql.get_tables():
         print table, pgsql.get_columns(table)
+    
