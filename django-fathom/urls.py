79499a3fb1ac7cf203806aefc49f7f7c7b2cff48
gruszczy <gruszczy@localhost>
2010-12-21 16:21:03 +0100

    serving jquery

--- django-fathom/urls.py
+++ django-fathom/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +8,9 @@ urlpatterns = patterns('',
     (r'^inspection/', include('inspection.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+         {'document_root': 'static/'}),
+    )
