7a4c874afc22b89a7b34f3e14eb4514edae18157
gruszczy <gruszczy@localhost>
2010-12-21 23:20:32 +0100

    fathom, setup: adding additional information about author

--- setup_fathom.py
+++ setup_fathom.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
+#coding=utf-8
 
 from distutils.core import setup
 
-setup(name='fathom', version='0.1.0a', packages=['fathom'])
+setup(name='fathom', version='0.1.0a', packages=['fathom'], 
+      author="Filip Gruszczyński", author_email="gruszczy@gmail.com",
+      description="database inspection library")
