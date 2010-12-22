37d74f460609e0af182c0a2c46bf5747d3b070b8
gruszczy <gruszczy@localhost>
2010-12-22 16:24:02 +0100

    some more work with pyparsing

--- fathom/_sqlite.py
+++ fathom/_sqlite.py
@@ -3,7 +3,7 @@
 from pyparsing import (Literal, CaselessLiteral, Word, Upcase, delimitedList, 
                        Optional, Combine, Group, alphas, nums, alphanums, 
                        ParseException, Forward, oneOf, quotedString, 
-                       ZeroOrMore, restOfLine, Keyword)
+                       ZeroOrMore, restOfLine, Keyword, White)
 
 class CreateTableParser(object):
     
@@ -11,11 +11,12 @@ class CreateTableParser(object):
     create_token = Keyword("create", caseless=True)
     table_token = Keyword("table", caseless=True)    
     table_name = Word(alphas, alphanums).setResultsName('table')
-    columns = Word(alphas, alphanums).setResultsName('columns')
+    column_name = Word(alphas, alphanums).setResultsName('columns')
+    column_type = Word(alphas, alphanums)
     
     create_stmt = Forward()
-    create_stmt << (create_token + table_token + table_name + '(' + columns + 
-                    ')' )
+    create_stmt << (create_token + table_token + table_name + '(' + 
+					ZeroOrMore(column_name + column_type) + ')' )
     
     def __init__(self):
         super(CreateTableParser, self).__init__()
@@ -23,9 +24,9 @@ class CreateTableParser(object):
     def parse(self, sql):
         try:
             tokens = self.create_stmt.parseString(sql)
-            print 'tokens=', tokens.table
+            print tokens.table, tokens.columns
         except ParseException as error:
             print error
             
 if __name__ == "__main__":
-    CreateTableParser().parse('create table dupa (x int)')
+    CreateTableParser().parse('create table dupa (x int z varchar)')
