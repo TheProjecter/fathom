92a9a336639e1130ad59d36bcb3d89708f412ed8
gruszczy <gruszczy@localhost>
2010-12-23 17:34:57 +0100

    fathom, sqlite: translating create table sqlite grammar to pyparsing

--- fathom/_sqlite.py
+++ fathom/_sqlite.py
@@ -3,59 +3,91 @@
 from pyparsing import (Literal, CaselessLiteral, Word, Upcase, delimitedList, 
                        Optional, Combine, Group, alphas, nums, alphanums, 
                        ParseException, Forward, oneOf, quotedString, 
-                       ZeroOrMore, restOfLine, Keyword, White, Suppress)
+                       ZeroOrMore, restOfLine, Keyword, White, Suppress,
+                       OneOrMore, StringEnd)
 
 class CreateTableParser(object):
+
+    # keywords:
+    CREATE = Keyword('CREATE', caseless=True)
+    TEMP = Keyword('TEMP', caseless=True)
+    TEMPORARY = Keyword('TEMPORARY', caseless=True)
+    TABLE = Keyword('TABLE', caseless=True)
+    IF = Keyword('IF', caseless=True)
+    NOT = Keyword('NOT', caseless=True)
+    EXISTS = Keyword('EXISTS', caseless=True)
+    AS = Keyword('AS', caseless=True)
+    keywords_set = (CREATE | TEMP | TEMPORARY | TABLE | IF | NOT | EXISTS | AS)
     
-    identifier = Combine(Optional('"') + Word(alphanums) + 
-                         ZeroOrMore('_' + Word(alphanums)) + Optional('"'))
-    identifiers = ((ZeroOrMore(identifier + ",") + Optional(identifier)) | 
-                   Optional(identifier))
-                    
+    # different types of identifier objects
+    identifier = (Combine(Optional('"') + Word(alphanums) + 
+                          ZeroOrMore('_' + Word(alphanums)) + 
+                          Optional('"')) & ~keywords_set)
+    identifier_coma = ((ZeroOrMore(identifier.copy() + ",") + Optional(identifier.copy())) | 
+                      Optional(identifier.copy()))
+    identifier_white = OneOrMore(identifier.copy())
         
-    create_token = Keyword("create", caseless=True)
-    table_token = Keyword("table", caseless=True)    
-    table_name = Combine(Optional('"') + Word(alphanums) + 
-                         ZeroOrMore('_' + Word(alphanums)) + Optional('"'))
-    table_name = table_name.setResultsName('table')
+    # statements used in sqlite 'CREATE TABLE' reference
+    create_table_stmt = Forward()
+    select_stmt = Forward()
+    column_def = Forward()
+    type_name = Forward()
+
+    # special statements for multiple occurance of certain statement
+    multi_column_def = Forward()
+    multi_table_constraint = Forward()
+    multi_column_constraint = Forward()
+
+    # identifiers
+    database_name = identifier.setResultsName('database_name', 
+                                              listAllMatches=True)
+    table_name = identifier.setResultsName('table_name', listAllMatches=True)
+    column_name = identifier.setResultsName('column_names', 
+                                            listAllMatches=True)
     
-    column_name = Combine(Optional('"') + Word(alphanums) +
-                          ZeroOrMore('_' + Word(alphanums)) + Optional('"'))
-    column_name = column_name.setResultsName('column_names', 
-                                             listAllMatches=True)
-    column_type = Combine(Word(alphas, alphanums) + 
-                   Optional("(" + Word(alphanums) + ")"))
-    not_null = (Keyword("not", caseless=True) +
-                Keyword("null", caseless=True))
-    primary_key = (Keyword("primary", caseless=True) +
-                   Keyword("key", caseless=True))
-    references = (Keyword("references", caseless=True) + identifier.copy() +
-                  Combine("(" + identifier.copy() + ")"))
-    unique = Keyword("unique", caseless=True)
-    
-    column = (column_name + column_type + Optional(not_null) +
-              Optional(primary_key) + Optional(references) + Optional(unique))
-    columns = ZeroOrMore(column + ',') + Optional(column)
-    
-    unique = Keyword("UNIQUE", caseless=True) + "(" + identifiers.copy() + ")"
-    unique = unique.setResultsName("uniques", listAllMatches=True)
-    table_constraints = ZeroOrMore(unique + ",") + Optional(unique)
-    
-    create_stmt = Forward()
-    create_stmt << (create_token + table_token + table_name + "(" + columns +
-                    table_constraints + ")")
-    
+    # building statement as described in sqlite 'CREATE TABLE' reference        
+    create_table_stmt << (CREATE + Optional(TEMP | TEMPORARY) + TABLE + 
+                          Optional(IF + NOT + EXISTS) + 
+                          Optional(database_name + '.') + table_name +
+                          ((AS + select_stmt) | 
+                           ('(' + multi_column_def + 
+                            Optional(multi_table_constraint) + ')')) + 
+                           StringEnd())
+    multi_column_def << ((OneOrMore(column_def + ',') + column_def) |
+                         Optional(column_def))
+                                   
+    column_def << column_name + Optional(type_name)
+        
     def __init__(self):
         super(CreateTableParser, self).__init__()
 
     def parse(self, sql):
         try:
-            tokens = self.create_stmt.parseString(sql)
+            tokens = self.create_table_stmt.parseString(sql)
+            print tokens.database_name, tokens.table_name, tokens.column_names
             return tokens.column_names
         except ParseException as error:
             print error
             
 if __name__ == "__main__":
+    CreateTableParser().parse('''
+CREATE TABLE "django"."django_site" (
+)''')
+    CreateTableParser().parse('''
+CREATE TABLE "django"."django_site" (
+)''')
+    CreateTableParser().parse('''
+CREATE TABLE "django"."django_site" (
+x
+)''')
+    CreateTableParser().parse('''
+CREATE TABLE "django"."django_site" (
+x, y,
+z,
+tytww_wer,
+rew
+)''')
+
     CreateTableParser().parse('''
 CREATE TABLE "django_site" (
 "id" integer NOT NULL PRIMARY   KEY,
