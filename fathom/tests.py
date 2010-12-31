f9e92627741dfa3a9ada3b99a5a9c6cf42ee1242
gruszczy <gruszczy@localhost>
2010-12-31 01:06:34 +0100

    fathom, tests: base test infrastructure

--- fathom/tests.py
+++ fathom/tests.py
@@ -1,25 +1,114 @@
 #!/usr/bin/python
 
+from abc import ABCMeta, abstractmethod
 from unittest import TestCase, main
 
-from _sqlite import CreateTableParser
-from inspectors import PostgresInspector
+from inspectors import PostgresInspector, SqliteInspector
 
-class PostgresTest(TestCase):
+try:
+    import psycopg2
+    TEST_POSTGRES = True
+except ImportError:
+    print('Failed to import psycopg2; skipping postgres tests.')
+    TEST_POSTGRES = False
     
+try:
+    import sqlite3
+    TEST_SQLITE = True
+except:
+    print('Failed to import sqlite3; skipping sqlite tests.')
+    TEST_SQLITE = False
+TEST_SQLITE = True # for time being
+
+class AbstractDatabaseTestCase:
+    
+    __metaclass__ = ABCMeta
+    
+    TABLES = {
+        'one_column': '''CREATE TABLE one_column ("column" varchar(800))''',
+        'one_unique_column': '''CREATE TABLE one_unique_column
+                                    ("column" integer UNIQUE)'''
+    }
+
     def setUp(self):
-        self.inspector = PostgresInspector('dbname=django user=django')
-    
+        # should be moved to setUpClass when python 2.7 is more popular
+        try:
+            self._add_tables()
+        except self.DatabaseErrors:
+            self.tearDown()
+            raise
+        
+    def tearDown(self):
+        self._drop_tables()
+
     def test_table_names(self):
-        tables = set(('auth_group_permissions', 'auth_group', 
-                      'auth_user_user_permissions', 'auth_user_groups',
-                      'auth_message', 'auth_user', 'auth_permission',
-                      'django_content_type', 'django_session', 'django_site'))
+        tables = set(self.TABLES.keys())
         self.assertEqual(set(self.inspector._get_tables()), tables)
         
     def test_view_names(self):
-        views = set(('au',))
+        views = set()
         self.assertEqual(set(self.inspector._get_views()), views)
+
+    # protected:
+    
+    @abstractmethod
+    def _get_connection(self):
+        pass
         
+    def _add_tables(self):
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        for stmt in self.TABLES.values():
+            cursor.execute(stmt);
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def _drop_tables(self):
+        # should be moved to setUpClass when python 2.7 is more popular
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        for name in self.TABLES:
+            try:
+                cursor.execute('DROP TABLE %s' % name);
+            except self.DatabaseErrors:
+                pass # maybe it was not created, but we need to try drop other
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+if TEST_POSTGRES: # turn to skip, when python 2.7 is more popular
+    class PostgresTestCase(AbstractDatabaseTestCase, TestCase):
+        
+        DBNAME = 'fathom'
+        USER = 'fathom'
+        
+        TABLES = AbstractDatabaseTestCase.TABLES.copy()
+        TABLES['empty'] = '''CREATE TABLE empty()'''
+    
+        def __init__(self, *args, **kwargs):
+            TestCase.__init__(self, *args, **kwargs)
+            args = self.DBNAME, self.USER
+            self.inspector = PostgresInspector('dbname=%s user=%s' % args)
+            self.DatabaseErrors = (psycopg2.ProgrammingError,
+                                   psycopg2.OperationalError)
+            
+        def _get_connection(self):
+            args = self.DBNAME, self.USER
+            return psycopg2.connect('dbname=%s user=%s' % args)
+
+
+if TEST_SQLITE: # turn to skip, when python 2.7 is more popular
+    class SqliteTestCase(AbstractDatabaseTestCase, TestCase):
+        
+        def __init__(self, *args, **kwargs):
+            TestCase.__init__(self, *args, **kwargs)
+            self.inspector = SqliteInspector('fathom.db3')
+            self.DatabaseErrors = (sqlite3.OperationalError, 
+                                   sqlite3.ProgrammingError)
+
+        def _get_connection(self):
+            return sqlite3.connect('fathom.db3')
+    
 if __name__ == "__main__":
     main()
