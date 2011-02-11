9b7227918a985f64dac64eeb3562ef33a8a88a92
gruszczy <gruszczy@localhost>
2011-02-11 18:51:14 +0100

    new interface; library provides simply a Database object, that knows how to fill itself with information; inspectors are invisible to user

--- fathom/__init__.py
+++ fathom/__init__.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
+from schema import Database
 from inspectors import PostgresInspector, SqliteInspector
+
+def get_sqlite3_database(path):
+    return Database(inspector=SqliteInspector(path))
+    
+def get_postgresql_database(args):
+    return Database(inspector=PostgresInspector(args))
--- fathom/schema.py
+++ fathom/schema.py
@@ -2,15 +2,41 @@
 
 class Database(object):
     
-    def __init__(self, name=''):
+    def __init__(self, name='', inspector=None):
         # TODO: somehow database name should be set too, maybe inspector should
         # get it too
         super(Database, self).__init__()
         self.name = name
-        self.tables = dict()
-        self.views = dict()
-        self.indices = dict()
-        self.stored_procedures = dict()
+        self.inspector = inspector
+
+        self._tables = None
+        self._views = None
+
+    def _refresh_tables(self):
+        if self.inspector is not None:
+            self._tables = self.inspector.get_tables()
+        else:
+            self._tables = []
+        
+    def _get_tables(self):
+        if self._tables is None:
+            self._refresh_tables()
+        return self._tables
+    
+    tables = property(_get_tables)
+    
+    def _refresh_views(self):
+        if self.inspector is not None:
+            self._views = self.inspector.get_views()
+        else:
+            self._views = []
+    
+    def _get_views(self):
+        if self._views is None:
+            self._refresh_views()
+        return self._views
+        
+    views = property(_get_views)
         
     def add_table(self, name):
         self.tables[name] = Table(name)
