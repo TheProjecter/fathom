28f7be32706609264094ab67968a1d8a6ae637cc
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-03-20 20:47:29 +0100

    setup prepared for 0.1.0

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@
 
 from distutils.core import setup
 
-setup(name='fathom', version='0.1.0a', packages=['fathom'], 
+setup(name='fathom', version='0.1.0', packages=['fathom'], 
       author="Filip Gruszczyński", author_email="gruszczy@gmail.com",
-      description="database inspection library")
+      description="database inspection library",
+      url='http://code.google.com/p/fathom',
+      classifiers = ["Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Database",
+        ])
