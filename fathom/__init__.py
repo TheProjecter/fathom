617ea451f0e4cf77b3db895e7af50d0f8558f857
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-03-26 12:26:50 +0100

    tests for getting database type

--- fathom/__init__.py
+++ fathom/__init__.py
@@ -2,6 +2,7 @@
 
 from .schema import Database
 from .inspectors import PostgresInspector, SqliteInspector, MySqlInspector
+from .errors import FathomError
 
 def get_sqlite3_database(path):
     return Database(inspector=SqliteInspector(path))
@@ -11,3 +12,6 @@ def get_postgresql_database(args):
 
 def get_mysql_database(**kwargs):
     return Database(inspector=MySqlInspector(**kwargs))
+
+def get_database_type(*args, **kwargs):
+    pass
