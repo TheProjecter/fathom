65403a8b2b55f619710fcb9242dac3c31f52f975
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-04-08 13:39:52 +0200

    fathom2django: parsing command line options

--- tools/fathom2django.py
+++ tools/fathom2django.py
@@ -6,9 +6,18 @@ import fathom
 
 DESCRIPTION = 'Build django models from database schema.'
 
+def database2django():
+    pass
+
+def table2django():
+    pass
+
+def fathom2django(args):
+    pass
+
 def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="database_type")
     
     postgres = subparsers.add_parser('postgresql')
     postgres.add_argument('connection', type=str, help='connection string')
@@ -23,7 +32,8 @@ def main():
     mysql.add_argument('-H', '--host', type=str, help='database host')
     mysql.add_argument('-P', '--port', type=str, help='database port')
     
-    parser.parse_args()
+    args = vars(parser.parse_args())
+    fathom2django(args)
 
 if __name__ == "__main__":
     main()
