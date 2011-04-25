b8cd8e22a3437afa9e3a6608c37ea9863b8b59b6
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-04-25 20:07:32 +0200

    setting some very simple names of databases

--- fathom/__init__.py
+++ fathom/__init__.py
@@ -7,13 +7,13 @@ from .errors import FathomError
 from .utils import get_database_type, find_accessing_procedures
 
 def get_sqlite3_database(path):
-    return Database(inspector=SqliteInspector(path))
+    return Database(name=path, inspector=SqliteInspector(path))
     
 def get_postgresql_database(args):
-    return Database(inspector=PostgresInspector(args))
+    return Database(name=args, inspector=PostgresInspector(args))
 
 def get_mysql_database(**kwargs):
-    return Database(inspector=MySqlInspector(**kwargs))
+    return Database(name=kwargs['db'], inspector=MySqlInspector(**kwargs))
     
 def get_oracle_database(user, password):
     return Database(inspector=OracleInspector(user, password))
