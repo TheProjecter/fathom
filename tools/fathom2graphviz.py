5b0ef7fbae85a6dedd5e74111f7a1486d5c46f42
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-05-05 14:30:13 +0200

    fathom2graphviz prints output to a file

--- tools/fathom2graphviz.py
+++ tools/fathom2graphviz.py
@@ -5,16 +5,23 @@ from fathom.utils import FathomArgumentParser
 DESCRIPTION = 'Build graphviz ER diagrams from database schema.'
 
 def database2graphviz(db, args):
-    print("digraph G {")
-    for table in db.tables.values():
-        table_node(table, args)
-    for table in db.tables.values():
-        table_connections(table)
-    print("}")
+    result = "digraph G {\n\n"
+    table_nodes = [table_node(table, args) for table in db.tables.values()]
+    result += '\n'.join(table_nodes) + '\n\n'
+    all_table_connections = [table_connections(table) 
+                             for table in db.tables.values()]
+    result += '\n'.join([connection for connection in all_table_connections
+                                    if connection]) + '\n\n'
+    result += "}\n"
+    if args.output is not None:
+        with open(args.output, 'w') as file:
+            file.write(result)
+    else:
+        print(result)
 
 def table_node(table, args):
     if not args.include_columns:
-        print(' "%s"[shape=box];' % table.name)
+        return ' "%s"[shape=box];' % table.name
     else:
         columns = []
         for column in table.columns.values():
@@ -23,17 +30,20 @@ def table_node(table, args):
         columns = ''.join(columns)
         label = '<table><tr><td bgcolor="lightgrey">%s</td></tr>%s</table>' % \
                 (table.name, columns)
-        print(' "%s"[shape=none,label=<%s>];' % (table.name, label))                
+        return ' "%s"[shape=none,label=<%s>];' % (table.name, label)
 
 def table_connections(table):
+    result = []
     for fk in table.foreign_keys:
         args = (table.name, fk.columns[0], fk.referenced_table, 
                 fk.referenced_columns[0])
-        print(' %s:%s -> %s:%s;' % args)
+        result.append(' %s:%s -> %s:%s;' % args)
+    return '\n'.join(result)
 
 def main():
     parser = FathomArgumentParser(description=DESCRIPTION)
     parser.add_argument('--include-columns', action='store_true', default=False)
+    parser.add_argument('-o', '--output', help='print output to a file')
     db, args = parser.parse_args()
     database2graphviz(db, args)
 
