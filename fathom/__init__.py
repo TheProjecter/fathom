100c3ae862cc79315ecf69e29d8ccaad78e7039b
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-06-12 17:23:12 +0200

    cleaning up tests

--- fathom/__init__.py
+++ fathom/__init__.py
@@ -4,7 +4,6 @@ from .schema import Database
 from .inspectors import (PostgresInspector, SqliteInspector, MySqlInspector,
                          OracleInspector)
 from .errors import FathomError
-from .utils import get_database_type, find_accessing_procedures
 
 def get_sqlite3_database(path):
     return Database(name=path, inspector=SqliteInspector(path))
@@ -17,14 +16,3 @@ def get_mysql_database(**kwargs):
     
 def get_oracle_database(user, password):
     return Database(inspector=OracleInspector(user, password))
-
-TYPE_TO_FUNCTION = {
-    'Sqlite3': get_sqlite3_database,
-    'PostgreSQL': get_postgresql_database,
-    'MySQL': get_mysql_database,
-    'Oracle': get_oracle_database
-}
-
-def get_database(*args, **kwargs):
-    function = TYPE_TO_FUNCTION[get_database_type(*args, **kwargs)]
-    return function(*args, **kwargs)
