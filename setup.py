596e2b2e13db744a1df7e369c3e332c286889271
Filip Gruszczy?ski <gruszczy@gmail.com>
2011-06-16 19:33:44 +0200

    needs to be 0.4.1 to upload to pypi again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from distutils.core import setup
 
-setup(name='fathom', version='0.4.0', packages=['fathom'], 
+setup(name='fathom', version='0.4.1', packages=['fathom'], 
       author="Filip Gruszczyński", author_email="gruszczy@gmail.com",
       description="database inspection library",
       url='http://code.google.com/p/fathom',
